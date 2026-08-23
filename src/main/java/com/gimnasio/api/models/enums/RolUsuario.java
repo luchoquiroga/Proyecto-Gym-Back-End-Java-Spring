7c44e0a607485c819bbfe8e0f8b44f93fbd73936
Luciano Quiroga <lucianoquiroga07@gmail.com>
2026-08-23 01:14:37 -0300

    modificacion de clases

---
+++ src/main/java/com/gimnasio/api/models/enums/RolUsuario.java
@@ -0,0 +1,9 @@
+package com.gimnasio.api.models.enums;
+
+/**
+ * Representa los roles de usuario permitidos para acceder y gestionar el sistema del gimnasio.
+ */
+public enum RolUsuario {
+    ADMIN,
+    GERENCIA
+}
